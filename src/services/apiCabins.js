bbf6ef2014a3a21e5831a3fc2c188f711ec07ee7
saad <saadarif.lgu@gmail.com>
2026-03-09 01:03:46 +0500

    connected supabase

---
+++ src/services/apiCabins.js
@@ -0,0 +1,12 @@
+import supabase from "./supabase";
+
+export async function getCabins() {
+  let { data, error } = await supabase.from("cabins").select("*");
+
+  if (error) {
+    console.error(error);
+    throw new Error("Failed to fetch cabins");
+  }
+
+  return data;
+}
